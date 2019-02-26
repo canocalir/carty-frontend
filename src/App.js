03956bc5e7a33015c0880e1dae43d78f8f83244b
Can Berk Ocalir <cocalir@gmail.com>
2019-02-26 15:06:19 +0300

    added heroku urls

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ onInputChange = (event) => {
 
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input});
-  fetch('http://localhost:3000/imageurl', {
+  fetch('https://enigmatic-spire-53847.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -71,7 +71,7 @@ onButtonSubmit = () => {
       .then(response => response.json())
       .then(response => {
      if (response) {
-       fetch('http://localhost:3000/image', {
+       fetch('https://enigmatic-spire-53847.herokuapp.com/image', {
         method: 'put',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
